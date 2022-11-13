0203eed865f0be6e1282ca387772e572e582bc6b
dict <mijiustad@gmail.com>
2022-11-13 20:35:32 +0500

    read from excel file2

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import pandas as pd
 
 def firstComeFirstServe():
-
     """
                   ================ First come first serve - FCFS - OS Scadualing Algorithm ================
     """
-    process = [1, 2, 3, 4]
-    time_taken_by_process = [12, 10, 5, 14]
+    df = pd.read_excel('myExcleFile.csv')
+    # process = [1, 2, 3, 4]
+    process = df[1].tolist()
+    # time_taken_by_process = [12, 10, 5, 14]
+    time_taken_by_process= df[2].tolist()
     waiting_time_of_process = 0
     count = 0
 
@@ -28,7 +31,6 @@ def firstComeFirstServe():
 
 
 def shortestJobFirst():
-
     """
                   ================ Shortest Job First - SRF - OS Scadualing Algorithm ================
     """
@@ -37,7 +39,7 @@ def shortestJobFirst():
     waiting_time_of_process = 0
     count = 0
 
-    print("\nexecution time of processes Before sorting to SJF => ", time_taken_by_process,'\n')
+    print("\nexecution time of processes Before sorting to SJF => ", time_taken_by_process, '\n')
     time_taken_by_process.sort()
 
     print("  Process", "\t", "Execution Time")
@@ -104,36 +106,98 @@ def priorityAlgorithm():
     print("Total waiting time => ", count, "ms")
     print("Average waiting time => ", str(count) + "/" + str(len(process)), "=", count / len(process), "ms")
 
+
+def findWaitingTime(processes, numberOfProcesses, executionTime, wt, quantum):
+    remainingTime = [0] * numberOfProcesses
+
+    # Copy the burst time into rt[]
+    for i in range(numberOfProcesses):
+        remainingTime[i] = executionTime[i]
+    currentTime = 0
+
+    # Keep traversing processes in round-robin manner until all of them are not done.
+    while 1:
+        done = True
+
+        # Traverse all processes one by
+        # one repeatedly
+        for i in range(numberOfProcesses):
+
+            # If burst time of a process is greater
+            # than 0 then only need to process further
+            if remainingTime[i] > 0:
+                done = False  # There is a pending process
+
+                if remainingTime[i] > quantum:
+
+                    # Increase the value of t i.e. shows
+                    # how much time a process has been processed
+                    currentTime = currentTime + quantum
+
+                    # Decrease the burst_time of current
+                    # process by quantum
+                    remainingTime[i] -= quantum
+
+                # If burst time is smaller than or equal
+                # to quantum. Last cycle for this process
+                else:
+
+                    # Increase the value of t i.e. shows
+                    # how much time a process has been processed
+                    currentTime = currentTime + remainingTime[i]
+
+                    # Waiting time is current time minus
+                    # time used by this process
+                    wt[i] = currentTime - executionTime[i]
+
+                    # As the process gets fully executed
+                    # make its remaining burst time = 0
+                    remainingTime[i] = 0
+
+        # If all processes are done
+        if done:
+            break
+
+# Function to calculate average waiting
+# and turn-around times.
+def findAverageTime(processes, n, executionTime, quantum):
+    waiting_time = [0] * n
+
+    # Function to find waiting time
+    # of all processes
+    findWaitingTime(processes, n, executionTime,
+                    waiting_time, quantum)
+
+
+    #Display processes along with all details
+
+    print("Processes Execution Time	 Waiting Time")
+    total_waiting_time = 0
+    total_tat = 0
+    for i in range(n):
+        total_waiting_time = total_waiting_time + waiting_time[i]
+        print(" ", i + 1, "\t\t", executionTime[i], "", "\t\t\t\t", waiting_time[i])
+
+    print("\nAverage waiting time = %.5f " % (total_waiting_time / n))
+
+
 def roundRobin():
     """
                       ================ Round Robin - OS Scadualing Algorithm ================
     """
-
+    # Process id's
     process = [1, 2, 3, 4]
-    time_taken_by_process = [12, 10, 5, 14]
-    waiting_time_of_process = 0
-    count = 0
+    n = 4
 
-    print("  Process", "\t", "Execution Time")
-    for j in range(len(process)):
-        print("\t", process[j], "\t\t\t", time_taken_by_process[j])
+    # Burst time of all processes
+    burst_time = [12, 10, 5, 14]
 
-    print("\n")
-
-    for i in range(len(process)):
-        print("The waiting time of process", process[i], "is =>", waiting_time_of_process, "ms")
-        waiting_time_of_process = waiting_time_of_process + time_taken_by_process[i]
-        print("The Execution time of process", process[i], "is =>", waiting_time_of_process, "ms", "\n")
-
-        if i < len(process) - 1:
-            count = count + waiting_time_of_process
-
-    print("Total waiting time => ", count, "ms")
-    print("Average waiting time => ", str(count) + "/" + str(len(process)), "=", count / len(process), "ms")
+    # Time quantum
+    quantum = 5
+    findAverageTime(process, n, burst_time, quantum)
 
 
 def menu():
-
     choose = 0
 
     while 1:
@@ -190,5 +254,4 @@ def menu():
 
 
 if __name__ == "__main__":
-
     menu()
